78f0819f79b2d678f5aee2836b93ca4e88f3fa3f
yxu-lanl <38266469+yxu-lanl@users.noreply.github.com>
2022-02-12 23:30:55 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   ‘recommonmark’,
-   ‘sphinx_markdown_tables’
+   'recommonmark',
+   'sphinx_markdown_tables'
 ]
 
 
